0ff55681258428a8384bb33c7d307fbed8b5ce8e
NiniolaX <loveafinni16@gmail.com>
2024-08-11 10:06:39 +0100

    Documented function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -6,4 +6,11 @@ them.
 
 
 def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings
+    Args:
+        str1(str): First string
+        str2(str): Second string
+    Return:
+        (str): Concatenation of two strings
+    """
     return str1 + str2
