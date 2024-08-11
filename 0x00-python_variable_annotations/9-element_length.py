c4477db68f277748314568bc586fd95de8738a47
NiniolaX <loveafinni16@gmail.com>
2024-08-12 00:41:49 +0100

    Added module containing function that returns a list of tuples of sequence and int

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""
+Contains a type-annoted function that takes in a list and returns a list of
+tuples.
+"""
+from typing import List, Tuple, Sequence, Iterable
+
+
+def element_length(lst: List[Iterable]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples
+    """
+    return [(i, len(i)) for i in lst]
