957e73a7d6464877cb31fdfddbe9b8f02d890479
NiniolaX <loveafinni16@gmail.com>
2024-08-11 10:03:57 +0100

    Added module containing function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Contains a type-annoted function that takes in two strings and concatenates
+them.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
