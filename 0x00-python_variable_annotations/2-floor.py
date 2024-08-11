c34111f182e9909317eae05a9aa47150cb08f023
NiniolaX <loveafinni16@gmail.com>
2024-08-11 10:27:34 +0100

    Added module containing function that floors a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Contains a function that takes a float n as argument and returns the floor
+of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floored value of a float
+    Args:
+        n(float): Number to floor
+    Return:
+        (int): Floored value of number
+    """
+    return math.floor(n)
