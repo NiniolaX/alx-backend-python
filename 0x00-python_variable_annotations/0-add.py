0b333e54a82602fcf0948be40c7e31748c76d014
NiniolaX <loveafinni16@gmail.com>
2024-08-11 09:55:08 +0100

    Wrote type-annoted function that adds two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Contains a type-annoted function that that takes a float a and a float b as
+arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two floats
+    Args:
+        a(float): First number
+        b(float): Second number
+    Return:
+        (float): Sum of two numbers
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,2 @@
+# 0X00 - PYTHON VARIABLE ANNOTATIONS
+This repository contains my code for the Python Variable Annotations project.
