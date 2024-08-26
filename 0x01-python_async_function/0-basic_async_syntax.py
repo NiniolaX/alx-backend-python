40b5e233cd464778fef7541b1e68ae582b86c50e
NiniolaX <loveafinni16@gmail.com>
2024-08-26 20:49:06 +0100

    Updated function to sort delays of coroutines without using sort()

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,10 +6,16 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> int:
     """
     Waits for a random delay between 0 and max_delay (included and float
-    value) and returns the duration of the delay
+    value) and returns the duration of the delay.
+
+    Args:
+        max_delay (int): The maximum delay value to pass to wait_random
+
+    Return:
+        int: Duration of delay
     """
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,10 +9,19 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns an asynchronous coroutine n times and returns a list of duration of
-    each coroutine in ascending order.
+    Spawns an asynchronous coroutine (wait_random) n times with the specified
+    max_delay and returns the list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay value to pass to wait_random.
+
+    Returns:
+        List[float]: List of delay times in ascending order.
     """
-    coroutines = [wait_random(max_delay) for i in range(n)]
-    delays = await asyncio.gather(*coroutines)
-    delays.sort()
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for coroutine in asyncio.as_completed(coroutines):
+        result = await coroutine
+        delays.append(result)
     return delays
