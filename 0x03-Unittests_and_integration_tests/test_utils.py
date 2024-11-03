e3c968a16ab8f1049fce7f8bf5e59f783365626c
NiniolaX <loveafinni16@gmail.com>
2024-11-03 09:59:10 +0100

    Added method that tests access_nested_map raises exception for invalid inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected_result: Union[int, Mapping]) -> None:
         """ Test access_nested_map returns expected ouput """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'")
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence,
+                                         expected_message: str) -> None:
+        """ Test access_nested_map raises exception for invalid inputs """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Check that exception message matches the expected message
+        self.assertEqual(str(context.exception), expected_message)
